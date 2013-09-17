5239232a5977adf38dd6d62f788e91c60257e23d
Sam Pierson <sam@sampierson.com>
2013-09-17 09:27:21 -0700

    rails new SamPierson --skip-test-unit --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamPierson::Application.config.secret_key_base = 'bb65425ba2cc8ecb3e6cf0c30a017c40b30b0cf94527fe6b728ba267a8d9ceaf9f01cf8e8051cf159aaad441332ca017cd1a478fdeb27b3dd0b8332b7e4114dd'
