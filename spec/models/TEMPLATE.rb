b98d01b88b22d868bd4d8b93f3c827e6e96df01b
Sam Pierson <sam@sampierson.com>
2013-12-09 20:02:50 -0800

    Model spec template

---
+++ spec/models/TEMPLATE.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Model (remove quotes)" do
+
+  describe "database" do
+    #it { should have_db_index(:foo_id).unique(true) }
+    #it { should have_foreign_key_for :foos, column: 'foo_id', name: 'foo_fk' }
+  end
+
+  describe "associations" do
+    #it { should belong_to(:foo).class_name('Foo').dependent(:destroy) }
+    #it { should have_many(:bar).through(:baz) }
+  end
+
+  describe "attributes" do
+    #it { should_not allow_mass_assignment_of :foo_id }
+    #it { should accept_nested_attributes_for :bar }
+  end
+
+  describe "validations" do
+    #it { should validate_presence_of :foo }
+  end
+
+  describe "authorization"
+
+  describe "lifecycle"
+
+  describe "class methods"
+
+  describe "instance methods"
+end
