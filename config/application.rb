ab1c3b331c8ef56e0faadd8379e07d4bc3b4f49a
Sam Pierson <sam@sampierson.com>
2013-12-17 14:36:43 -0800

    Autoload from lib/

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module SamPierson
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths += %W(#{config.root}/lib)
+
     config.i18n.enforce_available_locales = true # Avoid deprecation message
 
     config.generators do |g|
