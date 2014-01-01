be70f0b26fe04d2a85a9a6e4a3f3cd6fe39353f6
Sam Pierson <sam@sampierson.com>
2013-12-31 18:42:56 -0800

    Don't display 'Blog' heading on home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 module ApplicationHelper
 
   def site_title_helper
-    'SamPierson' + (Rails.env == 'production' ? '' : (' ' + Rails.env))
+    'SamPierson' + (Rails.env == 'production' ? '' : " #{Rails.env}")
+  end
+
+  def page_heading_helper
+    if content_for? :heading
+      content_for :heading
+    elsif @heading == false
+      # Suppress heading
+    else
+      heading = @heading || title || 'MISSING HEADING, SET title OR content_for :heading'
+      content_tag :h1, heading
+    end
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  describe "#page_heading_helper" do
+    subject { helper.page_heading_helper }
+
+    context "with no heading data" do
+      it "should display a missing message" do
+        expect(subject).to match /MISSING HEADING/
+      end
+
+      context "when there is a title set" do
+        before { helper.stub(:title) { 'test title' } }
+        it { should eq "<h1>test title</h1>" }
+
+        context "and @heading is set" do
+          before { @heading = 'at-heading' }
+          it { should eq "<h1>at-heading</h1>" }
+
+          context "and there is content_for :heading" do
+            before { helper.content_for :heading, "content for heading" }
+            it { should eq "content for heading" }
+          end
+        end
+      end
+
+      context "when @heading is false" do
+        before { @heading = false }
+
+        it "should suppress the heading" do
+          should be_nil
+        end
+      end
+    end
+  end
+end
