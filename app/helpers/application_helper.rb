bb621fd412a2acc46fe215e490a4e7250f40e5ce
Sam Pierson <sam@sampierson.com>
2014-01-01 22:09:20 -0800

    Display a random image behind the navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,12 @@ module ApplicationHelper
   def tick_helper(bool)
     bool ? "\u2713" : ''
   end
+
+  def navbar_random_image_helper
+    images = %w[beams chair corridor painted-desert-1 trees wall]
+    image = images.sample
+    content_tag :style do
+      "nav.navbar { background-image: url(\"assets/#{image}.jpg\");".html_safe
+    end
+  end
 end
