c4aae0da4eaf31ae60a04f84bfdecb671dbb9007
Sam Pierson <sam@sampierson.com>
2014-01-20 15:48:29 -0800

    lib/postgres.rb reek clean

--- lib/postgres.rb
+++ lib/postgres.rb
@@ -1,10 +1,31 @@
-module Postgres
+class Postgres
+
   def self.load(dumpfile)
-    db = YAML.load_file(Rails.root.join 'config/database.yml')[Rails.env]
-    command = "pg_restore --clean --no-owner -d #{db['database']}"
-    command += ENV['PW'] ? ' -W' : ' --no-password'
-    command += " -U #{db['username']}" unless db['username'].blank?
-    command += " #{dumpfile}"
-    execute command
+    execute pg_restore_command(dumpfile)
+  end
+
+  private
+
+  def self.pg_restore_command(dumpfile)
+    "pg_restore --clean --no-owner -d #{db_config['database']}" +
+      pg_restore_username_option +
+      pg_restore_password_option +
+      " #{dumpfile}"
+  end
+
+  def self.pg_restore_password_option
+    ENV['PW'] ? ' -W' : ' --no-password'
+  end
+
+  def self.pg_restore_username_option
+    username = db_config['username']
+    return "" if username.blank?
+    " -U #{username}"
+  end
+
+  def self.db_config
+    @db_config ||= begin
+      YAML.load_file(Rails.root.join 'config/database.yml')[Rails.env]
+    end
   end
 end
